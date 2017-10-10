5aaa17461d98bf2e2ae40a78a8f892f1babd7a63
Ashkan Abedi <ashkan.abedi@hotmail.com>
2017-10-10 10:48:17 +0330

    Commit 10/10/2017_10:48PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,13 @@ public class PlayerController : MonoBehaviour {
 
 	public float MoveSpeed;
 	public float RotSpeed;
+	public float JumpForce = 5f;
+	public float FallForce = 5f;
+	public float GroundedDist = 2.5f;
+	public bool IsGrounded = true;
+	public LayerMask GroundLayer;
 
+	private Vector3 Velocity = Vector3.zero;
 	private Transform ThisTransform = null;
 	private CharacterController ThisController = null;
 
@@ -20,16 +26,55 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate ()
+	{
 
-		float Horz = CrossPlatformInputManager.GetAxis("Horizontal");
-		float Vert = CrossPlatformInputManager.GetAxis("Vertical");
+		float Horz = CrossPlatformInputManager.GetAxis ("Horizontal");
+		float Vert = CrossPlatformInputManager.GetAxis ("Vertical");
+		Velocity.z = Vert * MoveSpeed;
 
-		ThisTransform.rotation *= Quaternion.Euler(0f, Horz * RotSpeed * Time.deltaTime, 0f);
+		ThisController.Move (ThisTransform.TransformDirection (Velocity) * Time.deltaTime);
+		ThisTransform.rotation *= Quaternion.Euler (0f, Horz * RotSpeed * Time.deltaTime, 0f);
 
 		//ThisTransform.position += ThisTransform.forward * Vert * MoveSpeed * Time.deltaTime;
-	
-		ThisController.SimpleMove(Vector3.forward * Vert * MoveSpeed);
+		//ThisController.SimpleMove(ThisTransform.forward * Vert * MoveSpeed);
+
+		//Are we Grounded?
+		if (DistanceToGround () < GroundedDist) {
+
+			IsGrounded = true;
+
+		} else {
+
+			IsGrounded = false;
+
+		}
+
+		//Jumping
+		if (CrossPlatformInputManager.GetAxis ("Jump") != 0 && IsGrounded == true) {
+
+			Velocity.y = JumpForce;
+
+		}
+
+		//Apply Gravity
+		Velocity.y += Physics.gravity.y * FallForce * Time.deltaTime;
+
+
 		
+	}
+
+	public float DistanceToGround ()
+	{
+		RaycastHit hit;
+		float distanceToGround = 0;
+		if (Physics.Raycast (ThisTransform.position, -Vector3.up , out hit, Mathf.Infinity, GroundLayer)) {
+
+			distanceToGround = hit.distance;
+			print (distanceToGround);
+		}
+
+		return distanceToGround;
+
 	}
 }
